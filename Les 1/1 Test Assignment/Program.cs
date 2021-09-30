efb2df7d810847e7c90b21ec7c5eccb1e06344ee
ThiboV-1952599 <71815432+ThiboV-1952599@users.noreply.github.com>
2021-09-30 19:43:01 +0200

    Les 1

---
+++ Les 1/1 Test Assignment/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1_Test_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstname = Console.ReadLine();
+            string lastname = Console.ReadLine();
+            Console.WriteLine("Hello " + firstname + " " + lastname + "!");
+        }
+    }
+}
---
+++ Les 1/Hello World/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            //Console.Write("Hallo");
+            //Console.WriteLine("Nog eens Hello");
+
+            string name = "Thibo";
+            name = "Jan";
+
+            string stad = "Mechelen";
+            
+            Console.WriteLine(name);
+
+            Console.WriteLine("Ik ben " + name + ", ik woon in " + stad);
+
+
+            string color;
+            Console.Write("Geef je lievelingskleur in: ");
+            color = Console.ReadLine();
+            Console.WriteLine("Jij koos voor " + color);
+            string dier;
+            Console.WriteLine("Geef je lievelingsdier in: ");
+            dier = Console.ReadLine();
+            Console.WriteLine("Jij koos voor ", dier);
+        }
+    }
+}
