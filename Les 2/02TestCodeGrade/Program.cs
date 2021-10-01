42e6e44619c7a506e6d616d97538387064c49639
ThiboV-1952599 <71815432+ThiboV-1952599@users.noreply.github.com>
2021-10-01 14:02:31 +0200

    Les 2

---
+++ Les 2/02TestCodeGrade/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _02TestCodeGrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int getal = Convert.ToInt32(Console.ReadLine());
+
+            int r0 = 0, r1 = 0, r2 = 0, r3 = 0, r4 = 0, r5 = 0, r6 = 0, r7 = 0;
+            r0 = getal % 2;
+            getal /= 2;
+            r1 = getal % 2;
+            getal /= 2;
+            r2 = getal % 2;
+            getal /= 2;
+            r3 = getal % 2;
+            getal /= 2;
+            r4 = getal % 2;
+            getal /= 2;
+            r5 = getal % 2;
+            getal /= 2;
+            r6 = getal % 2;
+            getal /= 2;
+            r7 = getal % 2;
+
+            Console.WriteLine("" + r7 + r6 + r5 + r4 + r3 + r2 + r1 + r0);
+        }
+    }
+}
---
+++ Les 2/Les 2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Les_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* VIARIABELEN DECLAREREN */
+            int getal;
+            byte eenByte;
+            double decimaal;
+            char c;
+
+            /* VARIABELEN INITIALISEREN */
+            getal = 69420;
+            //eenByte = getal; gaat nooit lukken
+            eenByte = 10;
+            getal = eenByte; //iets klein in iets groter steken lukt wel!
+
+            decimaal = 89.67;
+            //getal = decimaal; gaat nooit lukken, want integer is geen decimaal getal!
+
+            decimaal = getal; //iets zonder komma in iets met komma steken lukt wel!
+
+            c = 'c';
+            c = '6';
+            c = '^';
+
+
+            /* VARIABELE DECLAREREN & INITIALISEREN */
+            string text = "a"; //van zodra we dubbele quotes gebruiken, ook al is het maar één karakter, het is en blijft een string!
+
+            text = "Dit is tekst";
+
+            /* REKENKUNDIGE OPERATOREN */
+            Console.WriteLine("REKENKUNDIGE OPERATOREN");
+            Console.WriteLine(getal);
+            getal++;
+            Console.WriteLine(getal);
+            --getal;
+            Console.WriteLine(getal);
+
+            /* VERSCHIL TUSSEN PRE- & POSTFIX */
+            Console.WriteLine(getal++);
+            Console.WriteLine(++getal);
+            Console.WriteLine(getal--);
+            Console.WriteLine(--getal);
+
+            /* BINARY OPERATORS */
+            Console.WriteLine(10 + 3);
+            Console.WriteLine(10 - 3);
+            Console.WriteLine(10 * 3);
+            Console.WriteLine(10 / 3);
+            Console.WriteLine(10.0 / 3);
+            Console.WriteLine(10 % 3);
+
+            Console.WriteLine(10 - (10 / 3) * 3); // == modulo operator
+        }
+    }
+}
