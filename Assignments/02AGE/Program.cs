ae8613be8f1e322ada85587aac562ba97c072207
ThiboV-1952599 <71815432+ThiboV-1952599@users.noreply.github.com>
2021-10-02 07:55:26 +0200

    Assignments 02

---
+++ Assignments/02AGE/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _02AGE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            int birthyear = 2021 - age;
+            Console.WriteLine(name + " was born in " + birthyear + "!");
+        }
+    }
+}
---
+++ Assignments/02Celcius/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02Celcius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double fahrenheit = Convert.ToDouble(Console.ReadLine());
+            double celcius = (fahrenheit - 32) * 5 / 9;
+	    Console.WriteLine(Math.Round(celcius, 2));   	
+        }
+    }
+}
