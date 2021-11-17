c9797df5a57641f169b26cb45c3d8234f8962187
ThiboV-1952599 <71815432+ThiboV-1952599@users.noreply.github.com>
2021-11-17 17:56:54 +0100

    voorbeeldcode Les 7

---
+++ Les7/07Exeptions/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+
+namespace _07Exeptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //EXEPTION HANDLING MET IF-STRUCTUREN!!!
+            /*
+            Console.WriteLine("Geef aantal lijnen in: ");
+            string antwoord = Console.ReadLine();
+            int aantal;
+            if (!Int32.TryParse(antwoord, out aantal))
+            {
+                Console.WriteLine("Verkeerd getal ingegeven!");
+                return;
+            }
+
+            Console.Write("Geef een filenaam in: ");
+            string filenaam = Console.ReadLine();
+            if (!File.Exists(filenaam))
+            {
+                Console.WriteLine("File bestaat niet!");
+                return;
+            }
+
+            StreamReader stream = File.OpenText(filenaam);
+
+            for (int i = 0; i < aantal; i++)
+            {
+                Console.WriteLine(stream.ReadLine());
+            }
+            stream.Close();
+            */
+
+            try
+            {
+                Console.WriteLine("Geef aantal lijnen in: ");
+                int aantal = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Geef een filenaam in: ");
+                string filenaam = Console.ReadLine();
+
+                StreamReader stream = File.OpenText(filenaam);
+
+                for (int i = 0; i < aantal; i++)
+                {
+                    Console.WriteLine(stream.ReadLine());
+                }
+                stream.Close();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("U gaf een verkeerd getal in!");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Deze file bestaat niet!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("WHOOPIE!");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("+-----------+");
+                Console.WriteLine("|  THE END  |");
+                Console.WriteLine("+-----------+");
+            }
+
+
+
+
+
+
+            /*Console.WriteLine("Geef aantal lijnen in: ");
+            int aantal = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Geef een filenaam in: ");
+            string filenaam = Console.ReadLine();
+
+            StreamReader stream = File.OpenText(filenaam);
+
+            for (int i = 0; i < aantal; i++)
+            {
+                Console.WriteLine(stream.ReadLine());
+            }
+            stream.Close();*/
+        }
+    }
+}
---
+++ Les7/07TestCodeGrade/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace _07_Test_CodeGrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.OpenText("donkey.txt").ReadToEnd().ToLower();
+            string[] chars = Console.ReadLine().Split(' ');
+
+
+
+            if (chars.Length != 2)
+            {
+                Console.WriteLine("crazy input");
+                return;
+            }
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (!"azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN".Contains(Convert.ToChar(chars[i])))
+                {
+                    Console.WriteLine("crazy input");
+                    return;
+                }
+            }
+
+            Regex regex = new Regex(@"\b" + chars[0] + @"\w*" + chars[1] + @"\b");
+            MatchCollection matches = regex.Matches(text);
+
+            string[] words = new string[matches.Count];
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = matches[i].Value;
+            }
+
+            Array.Sort(words);
+
+            string word = "";
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (word != words[i])
+                {
+                    Console.Write(words[i] + " ");
+                    word = words[i];
+                }
+                
+
+            }
+        }
+    }
+}
