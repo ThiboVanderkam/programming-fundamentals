9c369ebd29100a157d87108e3b40754754f53d9c
ThiboV-1952599 <71815432+ThiboV-1952599@users.noreply.github.com>
2022-01-02 12:24:21 +0100

    taken les 8

---
+++ Assignments/08Assignments/08Occurences/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08Occurences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string input = Console.ReadLine();
+                string[] inputArray = input.Split(' ');
+                Dictionary<string, int> words = new Dictionary<string, int>();
+
+                for (int i = 0; i < inputArray.Length; i++)
+                {
+                    if (words.ContainsKey(inputArray[i]))
+                    {
+                        words[inputArray[i]]++;
+                    }
+                    else
+                    {
+                        words.Add(inputArray[i], 1);
+                    }
+                }
+
+                string[] keys = words.Keys.ToArray();
+                Array.Sort(keys);
+
+                foreach (var item in keys)
+                {
+                    Console.WriteLine(item + ": " + words[item]);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("crazy input");
+            }
+        }
+    }
+}
---
+++ Assignments/08Assignments/08Writing/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _08Writing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string filename = Console.ReadLine();
+                StreamReader stream = File.OpenText(filename);
+                string tekst = stream.ReadToEnd();
+                tekst = tekst.ToLower();
+                stream.Close();
+
+                string[] words = Console.ReadLine().Split(' ');
+
+                Dictionary<string, int> wordDict = new Dictionary<string, int>();
+
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (!wordDict.ContainsKey(words[i]))
+                    {
+                        wordDict.Add(words[i], 0);
+                    }
+                }
+
+                string cleantext = "";
+                for (int i = 0; i < tekst.Length; i++)
+                {
+                    if ("abcdefghijklmnopqrstuvwxyz ".Contains(tekst[i]))
+                    {
+                        cleantext += tekst[i];
+                    }
+                    else if ("\n".Contains(tekst[i]))
+                    {
+                        cleantext += " ";
+                    }
+                }
+
+                string[] cleanArray = cleantext.Split(' ');
+
+                for (int i = 0; i < cleanArray.Length; i++)
+                {
+                    if (wordDict.ContainsKey(cleanArray[i]))
+                    {
+                        wordDict[cleanArray[i]]++;
+                    }
+                }
+
+                string[] keys = wordDict.Keys.ToArray();
+                Array.Sort(keys);
+
+                foreach (var item in keys)
+                {
+                    if (wordDict[item] > 0)
+                    {
+                        Console.WriteLine(item + ": " + wordDict[item]);
+                    }
+                }
+
+                Console.Write("Not used: ");
+                foreach (var item in keys)
+                {
+                    if (wordDict[item] == 0)
+                    {
+                        Console.Write(item + " ");
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("crazy input");
+            }           
+        }
+    }
+}
