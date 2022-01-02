e61f0e18c0d50267ec8fb7b95091352ecd3b3e42
ThiboV-1952599 <71815432+ThiboV-1952599@users.noreply.github.com>
2022-01-02 12:23:54 +0100

    voorbeeldexamen

---
+++ Assignments/TrialExam/Trial1/Palindrome.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trial1
+{
+    class Palindrome
+    {
+        private string _InputNumber { get; set; }
+        public Palindrome(string number)
+        {
+            _InputNumber = number;
+        }
+
+        public bool CheckPalindrome(int number)
+        {
+            string stringNumber = Convert.ToString(number);
+            for (int i = 0; i < stringNumber.Length; i++)
+            {
+                if (!(stringNumber[i] == stringNumber[stringNumber.Length - 1 - i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public int FindNext()
+        {
+            int number = Convert.ToInt32(_InputNumber);
+            bool found = false;
+            while (!found)
+            {
+                number++;
+                found = CheckPalindrome(number);
+            }
+
+            return number;
+        }
+    }
+}
---
+++ Assignments/TrialExam/Trial1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Trial1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //ask for question
+                string question = Console.ReadLine();
+                if (question == "trial1")
+                {
+                    string name = Console.ReadLine();
+                    Trial1 t1 = new Trial1(name.ToUpper());
+                    t1.FindNames();
+                    Console.WriteLine(t1);
+                }
+                else if (question == "trial2")
+                {
+                    string number = Console.ReadLine();
+                    if (Convert.ToInt32(number) < 0)
+                    {
+                        Console.WriteLine("crazy input");
+                        return;
+                    }
+                    Palindrome pal = new Palindrome(number);
+                    Console.WriteLine(pal.FindNext());
+
+                }
+                else
+                {
+                    Console.WriteLine("crazy input");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("crazy input");
+            }
+        }
+    }
+}
